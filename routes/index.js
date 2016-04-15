dca44ab59d975cdd66f6a886005585611d7e11c5
LE Mai-Khanh <lemaikhanh.lmk@gmail.com>
2016-04-14 23:00:13 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  res.render('userlist', { title: 'userlist' });
+});
+
+router.post('/changePage', function(req, res) {
+	res.redirect("/helloworld");
+});
+
+module.exports = router;
