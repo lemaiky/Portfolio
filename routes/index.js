667621c25e9dc0089429a72cb723d16dabd10f96
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2017-01-13 15:05:50 +0100

    change site display

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mai-Khanh page' });
+  res.render('home', { title: 'Mai-Khanh page' });
 });
 
 /* GET Resume page. */
-router.get('/resume', function(req, res) {
-  res.render('resume', { title: 'Resume' });
-});
-
-/* GET Education page. */
-router.get('/education', function(req, res) {
-  res.render('education', { title: 'Education' });
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About' });
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res) {
   res.render('projects', { title: 'Projects' });
-});
-
-/* POST go to Hello World page*/
-router.post('/changePage', function(req, res) {
-	res.redirect("/resume");
 });
 
 /* POST redirect to linkedin */
