d1a9200cef14aba0a6100b2ce3c3559569ea621e
mataruga <danimataruga@gmail.com>
2018-11-13 14:43:05 +0100

    project list

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,13 +1,15 @@
 var client = d3.select(".right_bar").node().getBoundingClientRect();
-var marginProject = {top: 10, right: 30, bottom: 0, left: 0},
-    width = client.width,
+var width = client.width,
     height = client.height,
-    projectWidth = width / 4;
+    projectWidth = (width - (width*0.05)) / 5,
+    projectHeight = (height - (height*0.04)) / 4,
+    widthOffset = (width - (width*0.05)) / 4,
+    heightOffset = (height - (height*0.04)) / 3;
 
 var svgProjects = d3.select('.right_bar').append("svg")
     .attr("width", width)
     .attr("height", height)
-    .attr("transform", "translate(20, 35)");
+    .attr("transform", "translate(" + (width*0.05) + ", " + (height*0.04) + ")");
 
 d3.json("data/projects.json", function(error, data) {
 
@@ -15,20 +17,20 @@ d3.json("data/projects.json", function(error, data) {
             .data(data)
             .enter().append("g")
             .attr("class", function(d) { return "project " + d.name.replace(" ",""); })
-            .attr("transform", function(d,i) { return "translate(" + (272.5*(i%4)) + "," + (220*Math.floor(i/4)) + ")"; });
+            .attr("transform", function(d,i) { return "translate(" + (widthOffset*(i%4)) + "," + (heightOffset*Math.floor(i/4)) + ")"; })
+            .on("mouseover", projectMouseOver)
+            .on("mouseout", projectMouseOut);
 
     var project = svgProjects.selectAll(".project").append("rect")
-            .attr("width", 250)
-            .attr("height", 200)
+            .attr("width", projectWidth)
+            .attr("height", projectHeight)
             .attr("rx", 5)
             .attr("ry", 5);
 
     projectContainer.append("svg:image")
             .attr("class", "project_img")
-            .attr("height", "200")
-            .attr("width", "250")
-            .attr("rx", 10)
-            .attr("ry", 10)
+            .attr("height", projectHeight)
+            .attr("width", projectWidth)
             .attr("xlink:href", function(d) {return d.img;})
             .attr("preserveAspectRatio", "none");
 
@@ -41,3 +43,31 @@ d3.json("data/projects.json", function(error, data) {
     //             return d.name;
     //         });
 });
+
+function projectMouseOver(d) {
+    d3.select(this)
+        .transition()
+            .duration(100)
+            .style("opacity", .2);
+    
+    var fill = d3.select(this).append("text")
+        .attr("id", "project_name")
+        .attr("y", projectHeight/2)
+        .attr("x", projectWidth/2)
+        .style("text-anchor", "middle")
+        .attr("stroke", "black")
+        .text(function (d) { return d.name;});
+}
+
+function projectMouseOut(d) {
+    d3.select(this)
+        .transition()
+            .duration(100)
+            .style("opacity", 1);
+    
+    d3.select("#project_name").remove();
+}
+
+function wrap(text) {
+    
+}
--- javascripts/viz.js
+++ javascripts/viz.js
@@ -15,15 +15,22 @@ var xAxis = d3.axisTop()
         .scale(xScale)
         .ticks(5);
 
-d3.csv("data/skillsdata.csv", row, function(error, data) {
+var leftIndex = 0,
+    rightIndex = 0;
+
+d3.csv("data/skills.csv", row, function(error, data) {
     if (error) throw error;
 
     // Setup position for every datum; Applying different css classes to parents and leafs.
     var node = svgSkills.selectAll(".node")
             .data(data)
             .enter().append("g")
-            .attr("class", function(d) { return "node node--leaf"; })
-            .attr("transform", function(d,i) { return "translate(" + 15 + "," + (25*i+15) + ")"; });
+            .attr("class", function(d) { return "node node--leaf " + (d.value == 1 ? "node--leaf--left" : "node--leaf--right"); })
+            .attr("transform", function(d) { 
+                var xOffset = d.value == 1 ? widthOv*.03 : widthOv/2;
+                var i = d.value == 1 ? leftIndex++ : rightIndex++;
+                return "translate(" + xOffset + "," + (25*i+15) + ")"; 
+            });
 
     // Setup G for every leaf datum.
     var leafNodeG = svgSkills.selectAll(".node--leaf")
@@ -36,13 +43,10 @@ d3.csv("data/skillsdata.csv", row, function(error, data) {
     leafNodeG.append("rect")
             .attr("class","shadow")
             .style("fill", function(d) {return d.color;})
-            .attr("width", 2)
+            .attr("width", 125)
             .attr("height", 20)
             .attr("rx", 2)
-            .attr("ry", 2)
-            // .transition()
-            //     .duration(800)
-            //     .attr("width", function(d) {return xScale(d.value);});
+            .attr("ry", 2);
 
     leafNodeG.append("text")
             .attr("dy", 15)
@@ -64,11 +68,11 @@ function row(d) {
 }
 
 function onmouseover(d) {
-    if(d.color == "#F5CB5C") return;
     d3.selectAll('.project').filter(function(p) {return p.tags.indexOf(d.id) == -1;})
         .transition()
             .duration(100)
             .style("opacity", .2);
+    d3.select(this).select("rect").attr("stroke", "#F5CB5C").attr("stroke-width", 2.5);
 }
 
 function onmouseleave(d) {
@@ -76,4 +80,5 @@ function onmouseleave(d) {
         .transition()
             .duration(100)
             .style("opacity", 1);
+    d3.select(this).select("rect").attr("stroke", "none");
 }
